68a97b13cbbb1125afa2c01e604b6b69769cee25
Samart <ratsam93@gmail.com>
2024-03-01 22:42:00 +0530

    Update page.tsx

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from "next";
 import SectionPageTitle from "@/components/sections/section-page-title";
 import SectionChecklist from "@/components/sections/section-checklist";
 import SectionCTALayout4 from "@/components/sections/section-cta-layout-4";
-import SectionStats from "@/components/sections/section-stats";
-import SectionTeam from "@/components/sections/section-team";
+// import SectionStats from "@/components/sections/section-stats";
+// import SectionTeam from "@/components/sections/section-team";
 // import { getBaseUrl } from '@/getBaseUrl.js';
 
 // export const metadata: Metadata = {
@@ -21,15 +21,15 @@ export default function PageAbout() {
       <main className="relative">
         <SectionPageTitle
           subtitle="Focus on engaging, reusable content that decrease the cost per leads
-            while helps you to increase profits Opeyn. Opeyn strives to
+            while helps you to increase profits margin. Opeyn strives to
             deliver the tools and support that helps companies grow with
             unparalleled success."
         >
           About
         </SectionPageTitle>
         <SectionChecklist />
-        <SectionStats />
-        <SectionTeam />
+        {/* <SectionStats /> */}
+        {/* <SectionTeam /> */}
         <SectionCTALayout4 />
       </main>
       <Footer />
