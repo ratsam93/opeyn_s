0de4c09ca164aff2c9d6cb01046365209febb5d9
Samart <ratsam93@gmail.com>
2024-03-02 21:25:02 +0530

    update home meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,60 @@
+// import Header from "@/components/layout/header";
+// import Footer from "@/components/layout/footer";
+// import SectionCTA from "@/components/sections/section-cta";
+// import SectionHero from "@/components/sections/section-hero";
+// import SectionIconBoxes from "@/components/sections/section-icon-boxes";
+// // import SectionLatestNews from "@/components/sections/section-latest-news";
+// import SectionPartners from "@/components/sections/section-partners";
+// import SectionPromo from "@/components/sections/section-promo";
+// // import SectionTestimonialsSlider from "@/components/sections/section-testimonials-slider";
+// import type { Metadata } from "next";
+// import Head from 'next/head';
+
+
+// export const metadata: Metadata = {
+//   metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
+//   title: "OPEYN",
+//   description: "Home",
+// };
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Head>
+//         <title>OPEYN Marketing</title>
+//         <meta name="description" content="We provide cutting-edge digital marketing solutions, lead scoring techniques and comprehensive sales intelligence. Explore our range of SOPs and templates designed to streamline your processes." />
+//       </Head>
+//       <Header />
+//       <main className="relative">
+//         <SectionHero />
+//         <SectionIconBoxes />
+//         <SectionPromo />
+//         {/* <SectionTestimonialsSlider /> */}
+//         {/* <SectionLatestNews /> */}
+//         <SectionPartners />
+//         <SectionCTA />
+//       </main>
+//       <Footer />
+//     </>
+//   );
+// }
+
+
+
+
+
+
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import SectionCTA from "@/components/sections/section-cta";
 import SectionHero from "@/components/sections/section-hero";
 import SectionIconBoxes from "@/components/sections/section-icon-boxes";
-// import SectionLatestNews from "@/components/sections/section-latest-news";
 import SectionPartners from "@/components/sections/section-partners";
 import SectionPromo from "@/components/sections/section-promo";
-// import SectionTestimonialsSlider from "@/components/sections/section-testimonials-slider";
-import type { Metadata } from "next";
+import { NextPage } from 'next';
 import Head from 'next/head';
 
-
-export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
-  title: "OPEYN",
-  description: "Home",
-};
-
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <>
       <Head>
@@ -29,8 +66,6 @@ export default function Home() {
         <SectionHero />
         <SectionIconBoxes />
         <SectionPromo />
-        {/* <SectionTestimonialsSlider /> */}
-        {/* <SectionLatestNews /> */}
         <SectionPartners />
         <SectionCTA />
       </main>
@@ -38,3 +73,6 @@ export default function Home() {
     </>
   );
 }
+
+export default Home;
+
